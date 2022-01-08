96fbf087994753c24d91355507524faa0ee020dc
kekkerslawl <braydj@gmail.com>
2022-01-07 20:34:59 -0600

    buttons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingComponent } from './components/booking/booking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'booking',
+    component: BookingComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
